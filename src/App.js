0ba3d4d3ea11daba080764de347630ed2542012f
Nikola Vrhovac <vrllee@gmail.com>
2021-10-23 12:56:01 +0200

    css-modules

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import './App.css';
-import './NewsletterOverride.css';
-import Newsletter from './Newsletter.cssStylesheets';
+import { ThemeProvider } from '@friendsofreactjs/react-css-themr';
+import Newsletter from './Newsletter.cssModules';
+import * as css from './NewsletterOverride.module.css';
+// import './NewsletterOverride.css'; // css stylesheet
 // import { ThemeProvider } from 'styled-components'; // css in js
 // import { ThemeProvider } from './ThemeContext'; // inline
 
-// const theme = {
-// 	header: {
-// 		fg: '#ff598a',
-// 	},
-// 	input: {
-// 		color: '#fff',
-// 		background: '#070222',
-// 		textAlign: 'center',
-// 	},
-// 	inputFocus: {
-// 		outline: '2px solid #5e9fff',
-// 	},
-// };
+const theme = {
+	Newsletter: css,
+	// header: {
+	// 	fg: '#ff598a',
+	// },
+	// input: {
+	// 	color: '#fff',
+	// 	background: '#070222',
+	// 	textAlign: 'center',
+	// },
+	// inputFocus: {
+	// 	outline: '2px solid #5e9fff',
+	// },
+};
 
 function App() {
 	return (
 		<div className='app__newsletter'>
-			{/* <ThemeProvider theme={theme}> */}
-			<Newsletter />
-			{/* </ThemeProvider> */}
+			<ThemeProvider theme={theme}>
+				<Newsletter />
+			</ThemeProvider>
 		</div>
 	);
 }
